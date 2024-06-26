31787b0420526f3582b2acca9ef70548da931a24
Tuan Luong <tdluong@crimson.ua.edu>
2024-06-26 14:59:54 -0500

    add function to plot arbitrary gaussian ellipsoid instead of just axis align

--- util/plot_3D_phd.m
+++ util/plot_3D_phd.m
@@ -1,21 +1,11 @@
 function plot_3D_phd (map_est, transparency)
-    sqrt_cov = map_est.max_likeli_gm_cov .^ 0.5;
     hold on
     for ii = 1:size(map_est.max_likeli_gm_inten,2)
         inten = map_est.max_likeli_gm_inten(1,ii)/2;
 
-        phd_mu_x = map_est.max_likeli_gm_mu(1,ii);
-        phd_mu_y = map_est.max_likeli_gm_mu(2,ii);
-        phd_mu_z = map_est.max_likeli_gm_mu(3,ii);
-
-        phd_std_x = sqrt_cov(1,1,ii);
-        phd_std_y = sqrt_cov(2,2,ii);
-        phd_std_z = sqrt_cov(3,3,ii);
-
-        [X,Y,Z] = ellipsoid(phd_mu_x, phd_mu_y, phd_mu_z, phd_std_x, phd_std_y, phd_std_z, 10);
-        C = ones(size(Z)) * inten;
-        surf(X,Y,Z,C,'EdgeColor','none','FaceColor','flat','FaceAlpha',transparency)
-        %surf(X,Y,Z,C,'FaceColor','flat','FaceAlpha',transparency)
+        h = plot_gaussian_ellipsoid(map_est.max_likeli_gm_mu(:,ii),...
+            map_est.max_likeli_gm_cov(:,:,ii), inten);
+        set(h,'facealpha',transparency,'EdgeColor','none','FaceColor','flat')
         clim([0,1.4])
     end
 end
